2e08fbfdbc1cda788651e6d9e547bf2340a84373
joedag32 <joedag32@yahoo.com>
2020-03-03 09:51:14 -0500

    updated html output and removed console.logs

--- areas-of-study-app.js
+++ areas-of-study-app.js
@@ -44,8 +44,7 @@ var areasOfStudyApp = new Vue({
                     for (let i = 0; i < messageArray.length; i++) {
                         // only compare if the word typed is more than 2 characters
                         if (messageArray[i].length > 2) {
-                            console.log("title: " + m.title + " arrayValue: " + messageArray[i]);
-                            if (m.title.toLowerCase().indexOf(messageArray[i]) > -1 || m.tags.toLowerCase().indexOf(this.message) > -1 || m.school.toLowerCase().indexOf(this.message) > -1) {
+                            if (m.title.toLowerCase().indexOf(messageArray[i]) > -1 || m.tags.toLowerCase().indexOf(messageArray[i]) > -1 || m.school.toLowerCase().indexOf(messageArray[i]) > -1) {
                                 return true;
                             }
                         }
